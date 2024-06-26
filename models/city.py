b11f69b26b3ae48f6bcaa35eb6cf1f98e7107d0b
Godwin Bruce <godwin.bruce@gmail.com>
2024-06-25 18:45:40 -0700

    console updates

--- models/city.py
+++ models/city.py
@@ -10,8 +10,8 @@ class City(BaseModel):
     A class to represent the various cities
     
     Attributes:
-        state_id: (str) -> State.id.
-        name: (str)
+        state_id (str): The State id.
+        name: (str): The name of the city
     """
 
     state_id = ""
